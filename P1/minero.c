1a93e143dd0f5825663b7789f7586a495119ded0
angela <angela.valderrama@estudiante.uam.es>
2023-02-08 01:20:17 +0100

    un poco de orden + pow + falta que terminen todos me lia el exit

--- P1/minero.c
+++ P1/minero.c
@@ -5,13 +5,14 @@
 #include <unistd.h>
 #include "pow.h"
 
-pthread_t* create_threads(int num_threads,int obj);
+int round_init(int num_threads,int obj);
 void *prueba_de_fuerza(void *obj);
 
 int main(int argc, char** argv) {
     long int test = 0;
     pthread_t** hilos = NULL;
     int rounds = 0, num_threads = 0, obj = 0;
+    int i = 0;
 
     if(argc != 4) {
         fprintf(stderr, "Error: debes escribir ./minero <numero de rondas> <numero de hilos> <objetivo> para iniciar el programa.\n");
@@ -22,15 +23,25 @@ int main(int argc, char** argv) {
     num_threads = atoi(argv[2]);
     obj = atoi(argv[3]);
     
-    create_threads(num_threads, obj);
+    for(i = 0; i < rounds; i++){
+        printf("Ronda %d empieza.\n", i+1);
+        printf("Objetivo: %d\n", obj);
+
+        obj = round_init(num_threads, obj);
+        printf("Objetivo de la siguiente ronda: %d\n", obj);
+        if(obj == -1) {
+            fprintf(stderr, "Error: no se pudo empezar la ronda.\n");
+            return -1;
+        }
+    }
     
-    return 1;
+    return 0;
 }
 
-pthread_t* create_threads(int num_threads, int obj) {
+int round_init(int num_threads, int obj) {
     pthread_t *hilos = NULL;
     int i, err, res;
-    long int thread_result;
+    int thread_result;
     
     /**Creo un array para guardar los hilos*/
     hilos = (pthread_t *)malloc(num_threads*sizeof(pthread_t));
@@ -43,7 +54,7 @@ pthread_t* create_threads(int num_threads, int obj) {
             fprintf(stderr, "pthread_create : %s\n", strerror(err));
             free(hilos);
 
-            return NULL;
+            return -1;
         }
     }
 
@@ -55,7 +66,7 @@ pthread_t* create_threads(int num_threads, int obj) {
             fprintf(stderr, "pthread_join : %s\n", strerror(err));
             free(hilos);
 
-            return NULL;
+            return -1;
         }
     }
 
@@ -65,22 +76,20 @@ pthread_t* create_threads(int num_threads, int obj) {
         printf("id: %ld\n", hilos[i]);
     }
 
-    return NULL;
-
+    return thread_result;
 }
 
 void *prueba_de_fuerza(void *obj) {
-    int res = 0;
-    int *pObj = obj;
-    
-    while (res != *pObj)
-    {
-        res = (rand() % (10000000 - 1)) + 1;
-        if(res == *pObj) {
-            printf("Hilo %ld encontro la solucion\n", pthread_self());
-            fflush(stdout);
 
-            return (void *)pthread_self();
+    int res = 0, i = 0;
+    int *pObj = (int *)obj;
+
+    while(res != *pObj){
+        i = rand() % POW_LIMIT-1;
+        res = pow_hash(i);
+        if(res == *pObj) {
+            printf("Hilo %ld encontro la solucion para %d: %d\n", pthread_self(), res, i);
+            exit(i);
         }
     }
     
