3b75d4659f84866daec4b174347e8f3e577a56ac
angela <angela.valderrama@estudiante.uam.es>
2023-02-11 16:36:16 +0100

    random number between 0 and POW_LIMIT-1 + some comments

--- P1/minero.c
+++ P1/minero.c
@@ -5,10 +5,10 @@
 #include <unistd.h>
 #include "pow.h"
 
+int solucion = -1;
+
 void round_init(int num_threads,int obj);
 void *prueba_de_fuerza(void *obj);
-
-int solucion = -1;
 
 int main(int argc, char** argv) {
     int rounds = 0, num_threads = 0, obj = 0;
@@ -16,22 +16,22 @@ int main(int argc, char** argv) {
 
     if(argc != 4) {
         fprintf(stderr, "Error: debes escribir ./minero <numero de rondas> <numero de hilos> <objetivo> para iniciar el programa.\n");
-        return -1;
+        return EXIT_FAILURE;
     }
 
-    /**Parse the input values*/
+    /* Parse the input values */
     rounds = atoi(argv[1]);
     num_threads = atoi(argv[2]);
     obj = atoi(argv[3]);
     
-    /**Lanzamos las rondas de minado*/
+    /* Lanzamos las rondas de minado */
     for(i = 0; i < rounds; i++){
         printf("Ronda %d empieza.\n------------------------\n", i+1);
         printf("Objetivo: %d\n", obj);
 
         round_init(num_threads, obj);
 
-        /**Se guarda la solucion*/
+        /* Se guarda la solucion */
         obj = solucion;
         solucion = -1;
        
@@ -40,7 +40,7 @@ int main(int argc, char** argv) {
 
     printf("\nMinado terminado.\n");
     
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 /**
@@ -52,34 +52,31 @@ int main(int argc, char** argv) {
  * @return 
 */
 void round_init(int num_threads, int obj) {
+
     pthread_t *hilos = NULL;
     int i, err;
     
-    /**Creo un array para guardar los hilos*/
+    /* Se crea un array para guardar los hilos */
     hilos = (pthread_t *)malloc(num_threads*sizeof(pthread_t));
     
-    /**Creo y lanzo los hilos*/
+    /* Se crean y se lanzan los hilos que ejecutarán la prueba de fuerza */
     for(i = 0; i < num_threads; i++) {
 
         err = pthread_create(&hilos[i], NULL, prueba_de_fuerza, &obj);
         if(err != 0) {
             fprintf(stderr, "pthread_create : %s\n", strerror(err));
-            
             free(hilos);
-
             return;
         }
     }
 
-    /**Espero que terminen los hilos*/
+    /* Se espera a que terminen los hilos*/
     for(i = 0; i < num_threads; i++) {
 
         err = pthread_join(hilos[i], NULL);
         if(err != 0) {
             fprintf(stderr, "pthread_join : %s\n", strerror(err));
-            
             free(hilos);
-
             return;
         }
     }
@@ -102,18 +99,25 @@ void *prueba_de_fuerza(void *obj) {
     int *pObj = (int *)obj;
 
     while(solucion == -1){
-        i = (rand() % (1 - POW_LIMIT + 1)) + 1;
-        res = pow_hash(i);
-        if(res == *pObj) {
-            printf("Hilo %ld encontro la solucion %d\n", pthread_self(), res);
-            
-            solucion = i;
 
+        /* Se genera un numero aleatorio entre 0 y POW_LIMIT-1 */
+        i = rand() % POW_LIMIT;
+
+        /* Se calcula el hash del numero aleatorio */
+        res = pow_hash(i);
+
+        /* Se comprueba si el hash es igual al objetivo */
+        if(res == *pObj) {
+            printf("Hilo %ld encontró la solución %d para el objetivo %i\n", pthread_self(), i, res);
+
+            /* Se guarda la solucion en una variable global para que
+               los demás hilos sepan que tienen que parar de minar */
+            solucion = i;
             return NULL;
         }
     }
     
-    printf("Hilo %ld NO encontro solucion, terminando...\n", pthread_self());
+    printf("Hilo %ld NO encontró solución, terminando...\n", pthread_self());
     
     return NULL;
 }
