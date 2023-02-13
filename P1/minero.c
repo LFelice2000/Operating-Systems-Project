083a7acedffd531ebcaa571f16489e4a6f033136
angela <angela.valderrama@estudiante.uam.es>
2023-02-13 18:32:05 +0100

    mining parallelized

--- P1/minero.c
+++ P1/minero.c
@@ -8,10 +8,10 @@
 #include "minero.h"
 
 int solucion = 0;
-int objetivo_comun = -1;
+int target;
 
 int main(int argc, char** argv) {
-    int rounds = 0, num_threads = 0, obj = 0;
+    int rounds = 0, num_threads = 0;
     int i = 0;
 
     if(argc != 4) {
@@ -20,21 +20,24 @@ int main(int argc, char** argv) {
     }
 
     /* Parse the input values */
-    obj = atoi(argv[1]);
+    target = atoi(argv[1]);
     rounds = atoi(argv[2]);
     num_threads = atoi(argv[3]);
     
     /* Lanzamos las rondas de minado */
     for(i = 0; i < rounds; i++){
         printf("Ronda %d empieza.\n------------------------\n", i+1);
-        printf("Objetivo: %d\n", obj);
+        printf("Objetivo: %d\n", target);
 
         round_init(num_threads);
 
         /* Se guarda la solucion */
         solucion = 0;
-       
-        printf("Objetivo de la siguiente ronda: %d\n\n", obj);
+        
+        if(i != rounds-1){
+            /* Se actualiza el objetivo */
+            printf("Objetivo de la siguiente ronda: %d\n\n", target);
+        }
     }
 
     printf("\nMinado terminado.\n");
@@ -65,17 +68,18 @@ void round_init(int num_threads) {
         return;
     }
 
-    init = (int)ceil((double)(18 - 1)/num_threads);
+    init = (int)floor((double)(POW_LIMIT)/num_threads)+1;
     end = init;
     
     /* Se crean y se lanzan los hilos que ejecutarán la prueba de fuerza */
     for(i = 0; i < num_threads; i++) {
 
-        thInfo[i].lower = i*init;
-        thInfo[i].upper = end-1;
-        end += end;
-        if(end > 18) {
-            end = 18;
+        thInfo[i].lower = init * i;
+        thInfo[i].upper = end - 1;
+
+        end += init;
+        if(end > POW_LIMIT) {
+            end = POW_LIMIT;
         }
 
         printf("Empiezo %d, termino %d\n", thInfo[i].lower, thInfo[i].upper);
@@ -108,7 +112,7 @@ void round_init(int num_threads) {
 /**
  * Funcion que realiza el proof of work.
  * 
- * @param obj objective of the proof of power function.
+ * @param info objective of the proof of power function.
  * 
  * @return 
 */
@@ -117,25 +121,27 @@ void *prueba_de_fuerza(void *info) {
     int res = 0, i = 0;
     info_hilo *thInfo = (info_hilo*)info;
 
+    /* Se comprueba si ya se ha encontrado la solucion */
+    i = thInfo->lower;
     while(solucion == 0){
 
-        srand(time(NULL));
-        /* Se genera un numero aleatorio entre 0 y POW_LIMIT-1 */
-        i = (rand() % (thInfo->upper - thInfo->lower + 1)) + thInfo->lower;
+        if(i > thInfo->upper) {
+            break;
+        }
 
         /* Se calcula el hash del numero aleatorio */
         res = pow_hash(i);
-
         /* Se comprueba si el hash es igual al objetivo */
-        if(res == objetivo_comun) {
+        if(res == target) {
             printf("Hilo %ld encontró la solución %d para el objetivo %i\n", pthread_self(), i, res);
-
             /* Se guarda la solucion en una variable global para que
                los demás hilos sepan que tienen que parar de minar */
             solucion = 1;
-            objetivo_comun = i;
+            target = i;
             return NULL;
         }
+
+        i++;
     }
     
     printf("Hilo %ld NO encontró solución, terminando...\n", pthread_self());
