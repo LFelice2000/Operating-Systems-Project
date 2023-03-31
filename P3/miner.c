6e166a973eb16e9ee2478b33aa31da40abd0e34a
angela <angela.valderrama@estudiante.uam.es>
2023-03-31 11:52:43 +0200

    initial commit P3

---
+++ P3/miner.c
@@ -0,0 +1,17 @@
+/**
+ * @file miner.c
+ * @author Luis Felice and Angela Valderrama
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-31
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
